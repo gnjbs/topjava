a39686135dfde459f2491db38550bd55c277b569
ArturDS <arturdsoma@gmail.com>
2016-03-26 16:00:20 +0300

    hw3 meal table

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,21 +1,28 @@
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS meals;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START 100000;
 
 CREATE TABLE users
 (
-  id         INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-  name       VARCHAR NOT NULL,
-  email      VARCHAR NOT NULL,
-  password   VARCHAR NOT NULL,
-  registered TIMESTAMP DEFAULT now(),
-  enabled    BOOL DEFAULT TRUE,
+  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name             VARCHAR              NOT NULL,
+  email            VARCHAR              NOT NULL,
+  password         VARCHAR              NOT NULL,
+  registered       TIMESTAMP           DEFAULT now(),
+  enabled          BOOL                DEFAULT TRUE,
   calories_per_day INTEGER DEFAULT 2000 NOT NULL
 );
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
-
+CREATE TABLE meals
+(
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  meal_date   TIMESTAMP NOT NULL,
+  description VARCHAR   NOT NULL,
+  calories    INTEGER
+);
 CREATE TABLE user_roles
 (
   user_id INTEGER NOT NULL,
