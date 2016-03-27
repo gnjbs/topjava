16b80bed7636552572c73c796eab9aee103f3efb
ArturDS <arturdsoma@gmail.com>
2016-03-27 23:57:46 +0300

    hw3 jdbcUserMealRepositoryImpl none complete

---
+++ src/test/java/ru/javawebinar/topjava/service/UserMealServiceTest.java
@@ -0,0 +1,73 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.util.DbPopulator;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+
+/**
+ * Created by ArturDS on 27.03.2016.
+ */
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserMealServiceTest {
+
+    @Autowired
+    protected UserMealService service;
+
+    @Autowired
+    private DbPopulator dbPopulator;
+
+    @Before
+    public void setUp() throws Exception {
+        dbPopulator.execute();
+    }
+
+    @Test
+    public void testGet() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+       //
+        service.delete(1,10000);
+    }
+
+    @Test
+    public void testGetBetweenDates() throws Exception {
+
+    }
+
+    @Test
+    public void testGetBetweenDateTimes() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testSave() throws Exception {
+        UserMeal um = new UserMeal(LocalDateTime.of(2016, Month.MARCH, 21, 10, 0), "Завтрак", 500);
+        service.save(um,10000);
+    }
+}
