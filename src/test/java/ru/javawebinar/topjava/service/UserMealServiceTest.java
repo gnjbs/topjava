ac0da823554fda95ab29c5a14d44fd531299d192
ArturDS <arturdsoma@gmail.com>
2016-03-29 23:14:19 +0300

    hw3 jdbcUserMealRepositoryImpl  between is ok

--- src/test/java/ru/javawebinar/topjava/service/UserMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/UserMealServiceTest.java
@@ -10,14 +10,18 @@ import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.util.DbPopulator;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
+import static junit.framework.Assert.assertEquals;
 import static ru.javawebinar.topjava.MealTestData.MATCHER;
 import static ru.javawebinar.topjava.MealTestData.MEAL_LIST;
 import static ru.javawebinar.topjava.UserTestData.ADMIN;
 import static ru.javawebinar.topjava.UserTestData.USER;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 /**
  * Created by ArturDS on 27.03.2016.
@@ -43,7 +47,7 @@ public class UserMealServiceTest {
 
     @Test
     public void testGet() throws Exception {
-        UserMeal um = service.get(100002,USER.getId());
+        UserMeal um = service.get(100002, USER.getId());
         MATCHER.assertEquals(MEAL_LIST.get(0), um);
     }
 
@@ -60,12 +64,19 @@ public class UserMealServiceTest {
 
     @Test
     public void testGetBetweenDates() throws Exception {
-
+        Collection<UserMeal> userMealCollection = service.getBetweenDates(LocalDate.of(2016, Month.MARCH, 21), LocalDate.of(2016, Month.MARCH, 21), USER.getId());
+        Collection<UserMeal> userMealsFromMealList = MEAL_LIST.stream()
+                .filter(um -> um.getDateTime().toLocalDate().equals(LocalDate.of(2016, Month.MARCH, 21))).collect(Collectors.toList());
+        assertEquals(userMealCollection, userMealsFromMealList);
     }
 
     @Test
     public void testGetBetweenDateTimes() throws Exception {
-
+        Collection<UserMeal> userMealCollection =
+                service.getBetweenDateTimes(LocalDateTime.of(2016, Month.MARCH, 21, 9, 0), LocalDateTime.of(2016, Month.MARCH, 21, 19, 0), USER.getId());
+        Collection<UserMeal> userMealsFromMealList = MEAL_LIST.stream()
+                .filter(um -> um.getDateTime().isBefore(LocalDateTime.of(2016, Month.MARCH, 21, 23, 59))).collect(Collectors.toList());
+        assertEquals(userMealCollection, userMealsFromMealList);
     }
 
     @Test
@@ -76,12 +87,14 @@ public class UserMealServiceTest {
 
     @Test
     public void testUpdate() throws Exception {
-
+        UserMeal um = new UserMeal(100002, LocalDateTime.of(2016, Month.MARCH, 21, 9, 30), "Завтрак", 500);
+        service.update(um, USER_ID);
+        MATCHER.assertEquals(um, service.get(100002, 100000));
     }
 
     @Test
     public void testSave() throws Exception {
         UserMeal um = new UserMeal(LocalDateTime.of(2016, Month.MARCH, 21, 0, 0), "Завтрак", 500);
-        service.save(um, 10000);
+        service.save(um, USER_ID);
     }
 }
